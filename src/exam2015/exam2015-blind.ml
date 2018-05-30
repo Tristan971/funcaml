84b95d851a661868a78df08ba8ab3f5da42eba0a
Tristan Deloche <tristandeloche@gmail.com>
2018-05-31 00:42:14 +0200

    Add testing for tide data parsing

--- src/exam2015/exam2015-blind.ml
+++ src/exam2015/exam2015-blind.ml
@@ -20,7 +20,7 @@ let split_lines (lines: (string list)) : (string list) list =
   let split_line (line: string) : string list = Str.split (Str.regexp " ") line
   in List.map split_line lines
 
-(* T 1.1 *)
+(* T1.1 *)
 let test_split: unit = 
   let test_lines : (string list) = [ "14/02 11:45 --:-- 6:03 18:29"; "15/02 0:09 12:37 6:51 19:20" ] in
   let res: (string list) list = split_lines test_lines in
@@ -31,6 +31,30 @@ let test_split: unit =
   print_string "Expected : "; print_list_list expected; print_char '\n'; 
   print_string "Got : "; print_list_list res; print_char '\n'
 
+(* Q1.2 *)
+type tide_data = {
+  date : string;
+  hmam : string;
+  hmpm : string;
+  bmam : string;
+  bmpm : string
+}
+
+let to_tide_data (line: (string list)) : tide_data =
+  match line with
+  | [d;h1;h2;h3;h4] -> { date=d; bmam=h1; hmam=h2; bmpm=h3; hmpm=h4 }
+  | _ -> failwith "bad line format"
+
+let to_tide_datas (lines: (string list) list) : (tide_data) list = 
+  List.map to_tide_data lines
+
+(* T1.2 *)
+let test_parse: unit =
+  let expected_result = to_tide_data [ "14/02"; "11:45"; "--:--"; "6:03"; "18:29" ] in
+  match expected_result with 
+    | {date;hmam;hmpm;bmam;bmpm} -> print_string "Success parse : OK"
 
 (* EXECTESTS *)
-let main: unit = test_split
+let main: unit = 
+  test_split; 
+  test_parse;
