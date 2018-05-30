f9cdb946b857e90b517abf7b1e31d5fc143834e5
Tristan Deloche <tristandeloche@gmail.com>
2018-05-31 01:36:35 +0200

    Add reordering

--- src/exam2015/exam2015-blind.ml
+++ src/exam2015/exam2015-blind.ml
@@ -1,6 +1,7 @@
 #load "str.cma"
 
-(* UTILS *)
+(* BEGIN_UTILS *)
+
 let print_list_str (l: (string list)): unit =
   let rec print_list_elem (li: ('a list)) = match li with
     | [] -> print_string "]"
@@ -14,6 +15,16 @@ let print_list_list (l: (string list) list): unit =
     | h::[] -> print_list_str h; print_char ']'
     | h::n::t -> print_list_str h; print_string ", "; print_list_elem (n::t)
   in print_char '['; print_list_elem l
+
+let expect_string_value (expected: string) (actual: string) : bool =
+  let cmpRes : int = String.compare expected actual in
+  if cmpRes == 0 then 
+    true 
+  else
+    let _ = Printf.printf "Actual != Expected! (CMP_RES = %i)\n\t-> Expected : %s\n\t-> Actual : %s\n" cmpRes expected actual in
+    false
+
+(* END_UTILS *)
 
 (* Q1.1 *)
 let split_lines (lines: (string list)) : (string list) list = 
@@ -62,6 +73,22 @@ let test_parse_list: bool =
     | d1::d2::[] -> true
     | _ -> false
 
+(* Q1.4 *)
+let reorder_data (tide_data: tide_data) : string = 
+  let sorted_hours : string = 
+    if (String.compare tide_data.bmam tide_data.hmam) < 0 then
+      Format.asprintf "%s(BM) %s(HM) %s(BM) %s(HM)" tide_data.bmam tide_data.hmam tide_data.bmpm tide_data.hmpm
+    else
+      Format.asprintf "%s(BM) %s(HM) %s(BM) %s(HM)" tide_data.hmam tide_data.bmam tide_data.hmpm tide_data.bmpm
+    in
+  Format.asprintf "%s %s" tide_data.date sorted_hours
+
+(* T1.4 *)
+let test_reorder_data : bool =
+  let expected : string = "15/02 0:09(HM) 6:51(BM) 12:37(HM) 19:20(BM)" in
+  let actual : string = reorder_data { date="15/02"; hmam="0:09"; hmpm="12:37"; bmam="6:51"; bmpm="19:20" } in
+  expect_string_value expected actual
+
 (* EXECTESTS *)
 let exec_test (name: string) (test_fn: bool) : unit =
   Printf.printf "%s -> %s" name (if test_fn then "OK" else "NOK"); 
@@ -69,6 +96,8 @@ let exec_test (name: string) (test_fn: bool) : unit =
 
 let main: unit = 
   print_string "====================== TESTS ======================\n";
+  flush stdout;
   exec_test "SPLIT_STR" test_split;
   exec_test "PARSE_SINGLE_TIDE_DATA" test_parse;
   exec_test "PARSE_MULTIPLE_TIDE_DATA_LINES" test_parse_list;
+  exec_test "REORDER_DATA" test_reorder_data;
