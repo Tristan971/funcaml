908c6a4dd99c01f9095d77439f53981e1e41bdd0
Tristan Deloche <tristandeloche@gmail.com>
2018-05-31 00:59:59 +0200

    Better test layout

--- src/exam2015/exam2015-blind.ml
+++ src/exam2015/exam2015-blind.ml
@@ -21,17 +21,16 @@ let split_lines (lines: (string list)) : (string list) list =
   in List.map split_line lines
 
 (* T1.1 *)
-let test_split: unit = 
+let test_split: bool = 
   let test_lines : (string list) = [ "14/02 11:45 --:-- 6:03 18:29"; "15/02 0:09 12:37 6:51 19:20" ] in
   let res: (string list) list = split_lines test_lines in
   let expected : (string list) list = [ 
     [ "14/02"; "11:45"; "--:--"; "6:03"; "18:29" ];
     [ "15/02"; "0:09"; "12:37"; "6:51"; "19:20" ] 
   ] in 
-  print_string "Expected : "; print_list_list expected; print_char '\n'; 
-  print_string "Got : "; print_list_list res; print_char '\n'
+  List.length res == List.length expected
 
-(* Q1.2 *)
+(* TYPE tide_data *)
 type tide_data = {
   date : string;
   hmam : string;
@@ -40,21 +39,36 @@ type tide_data = {
   bmpm : string
 }
 
+(* Q1.2 *)
 let to_tide_data (line: (string list)) : tide_data =
   match line with
   | [d;h1;h2;h3;h4] -> { date=d; bmam=h1; hmam=h2; bmpm=h3; hmpm=h4 }
   | _ -> failwith "bad line format"
 
+(* T1.2 *)
+let test_parse: bool =
+  let res = to_tide_data [ "14/02"; "11:45"; "--:--"; "6:03"; "18:29" ] in
+  match res with 
+    | {date;hmam;hmpm;bmam;bmpm} -> true
+
+(* Q1.3 *)
 let to_tide_datas (lines: (string list) list) : (tide_data) list = 
   List.map to_tide_data lines
 
-(* T1.2 *)
-let test_parse: unit =
-  let expected_result = to_tide_data [ "14/02"; "11:45"; "--:--"; "6:03"; "18:29" ] in
-  match expected_result with 
-    | {date;hmam;hmpm;bmam;bmpm} -> print_string "Success parse : OK"
+(* T1.3 *)
+let test_parse_list: bool =
+  let res = to_tide_datas [ [ "14/02"; "11:45"; "--:--"; "6:03"; "18:29" ];[ "15/02"; "0:09"; "12:37"; "6:51"; "19:20" ] ] in
+  match res with
+    | d1::d2::[] -> true
+    | _ -> false
 
 (* EXECTESTS *)
+let exec_test (name: string) (test_fn: bool) : unit =
+  Printf.printf "%s -> %s" name (if test_fn then "OK" else "NOK"); 
+  print_char '\n' 
+
 let main: unit = 
-  test_split; 
-  test_parse;
+  print_string "====================== TESTS ======================\n";
+  exec_test "SPLIT_STR" test_split;
+  exec_test "PARSE_SINGLE_TIDE_DATA" test_parse;
+  exec_test "PARSE_MULTIPLE_TIDE_DATA_LINES" test_parse_list;
